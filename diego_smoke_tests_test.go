0308be92d75251dc50f74079820c54b1bef0b6b1
Kris Hicks <krishicks@gmail.com>
2015-02-02 16:17:13 -0800

    test that app runs on diego after staging
    [#76629728]
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- diego_smoke_tests_test.go
+++ diego_smoke_tests_test.go
@@ -1,20 +1,30 @@
 package diego_smoke_tests_test
 
 import (
+	"os"
 	"time"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Staging", func() {
-	var appName string
+	var (
+		appName    string
+		appsDomain string
+		appRoute   string
+	)
 
 	BeforeEach(func() {
 		appName = generator.RandomName()
+		appsDomain = os.Getenv("SMOKE_TESTS_APPS_DOMAIN")
+		Ω(appsDomain).ShouldNot(BeEmpty(), "must set $SMOKE_TESTS_APPS_DOMAIN")
+		appRoute = "http://" + appName + "." + appsDomain + "/"
 	})
 
 	AfterEach(func() {
@@ -25,6 +35,13 @@ var _ = Describe("Staging", func() {
 	It("works", func() {
 		Eventually(cf.Cf("push", appName, "-p", "dora", "--no-start")).Should(gexec.Exit(0))
 		Eventually(cf.Cf("set-env", appName, "DIEGO_STAGE_BETA", "true")).Should(gexec.Exit(0))
+		Eventually(cf.Cf("set-env", appName, "DIEGO_RUN_BETA", "true")).Should(gexec.Exit(0))
 		Eventually(cf.Cf("start", appName), 5*time.Minute).Should(gexec.Exit(0))
+
+		Eventually(func() *gexec.Session {
+			curl := runner.Curl(appRoute)
+			curl.Wait()
+			return curl
+		}).Should(gbytes.Say("Hi, I'm Dora!"))
 	})
 })
